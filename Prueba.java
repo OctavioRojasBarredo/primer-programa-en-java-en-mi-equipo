5c44aa13400a1b0f5c3b48e469f2f7d04adec554
Octavio <octaviorojasbarredo@gmail.com>
2023-10-10 12:59:59 +0200

    Artista favorito

--- Prueba.java
+++ Prueba.java
@@ -2,9 +2,9 @@ public class Prueba {
 	
 	public static void main(String args[]){
 		
-		System.out.println("Rojas Barredo");
 		System.out.println("18 Años, 2005");
 		System.out.println("Tortilla");
+		System.out.println("Lofi Girl");
 		
 	}
 	
